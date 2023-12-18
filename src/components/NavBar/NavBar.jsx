e60c51e113a22aa514ab5fdc87a7ed21f14a4095
empostigo <emmanuel.postigo@tutanota.com>
2023-12-18 13:33:06 +0100

    Rename icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,40 @@
 // React
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 // Images
-import zen from "../../assets/navbar/zen.svg"
-import swimming from "../../assets/navbar/swimming.svg"
-import cycle from "../../assets/navbar/cycle.svg"
-import barebell from "../../assets/navbar/barebell.svg"
+import general from "../../assets/navbar/general.svg"
+import activity from "../../assets/navbar/activity.svg"
+import sessions from "../../assets/navbar/sessions.svg"
+import performance from "../../assets/navbar/performance.svg"
 
 // Style
 import navbarStyle from "./NavBar.module.scss"
 
 const NavBar = () => {
+  const { userId } = useParams()
+  console.log(userId)
+
   return (
-    <nav className={navbarStyle.nav}>
-      <Link to="#">
-        <img className={navbarStyle.img} src={zen} alt="Yoga" />
-      </Link>
-      <Link to="#">
-        <img className={navbarStyle.img} src={swimming} alt="Natation" />
-      </Link>
-      <Link to="#">
-        <img className={navbarStyle.img} src={cycle} alt="Vélo" />
-      </Link>
-      <Link to="#">
-        <img className={navbarStyle.img} src={barebell} alt="Musculation" />
-      </Link>
-    </nav>
+    <>
+      <nav className={navbarStyle.nav}>
+        <Link to={`/user/${userId}`}>
+          <img className={navbarStyle.img} src={general} alt="Yoga" />
+        </Link>
+        <Link to={`/user/${userId}/activity`}>
+          <img className={navbarStyle.img} src={activity} alt="Natation" />
+        </Link>
+        <Link to={`/user/${userId}/average-sessions`}>
+          <img className={navbarStyle.img} src={sessions} alt="Vélo" />
+        </Link>
+        <Link to={`/user/${userId}/performance`}>
+          <img
+            className={navbarStyle.img}
+            src={performance}
+            alt="Musculation"
+          />
+        </Link>
+      </nav>
+    </>
   )
 }
 
