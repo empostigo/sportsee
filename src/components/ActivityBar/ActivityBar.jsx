3cd1e1d95e08b7acce2c310e3cce71e02e45ead3
empostigo <emmanuel.postigo@tutanota.com>
2023-12-22 19:43:13 +0100

    Create ActivityBAr style

--- src/components/ActivityBar/ActivityBar.jsx
+++ src/components/ActivityBar/ActivityBar.jsx
@@ -1,55 +1,52 @@
 // Recharts
-import {
-  BarChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-  Bar
-} from "recharts"
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar } from "recharts"
 
 // Components
 import BarShape from "../BarShape/BarShape"
 import CustomTooltip from "../CustomTooltip/CustomTooltip"
 
+// Style
+import activityStyle from "./ActivityBar.module.scss"
+
 const ActivityBar = ({ data }) => {
   const xAxisValues = () => data.map(activity => data.indexOf(activity) + 1)
 
   return (
-    <BarChart width={730} height={250} data={data}>
-      <XAxis dataKey={xAxisValues} />
-      <YAxis
-        yAxisId="kg"
-        dataKey="kg"
-        type="number"
-        domain={["dataMin - 10", "dataMax + 10"]}
-        orientation="right"
-      />
-      <YAxis
-        yAxisId="cal"
-        dataKey="cal"
-        hide={true}
-        domain={["dataMin - 70", "dataMax + 70"]}
-      />
-      <Tooltip content={<CustomTooltip />} />
-      <Bar
-        dataKey="kg"
-        barSize={7}
-        fill="#282d30"
-        yAxisId="kg"
-        shape={<BarShape />}
-        activeBar={<BarShape />}
-      />
-      <Bar
-        dataKey="cal"
-        barSize={7}
-        fill="#e60000"
-        yAxisId="cal"
-        shape={<BarShape />}
-        activeBar={<BarShape />}
-      />
-    </BarChart>
+    <article className={activityStyle.article}>
+      <BarChart width={730} height={250} data={data}>
+        <XAxis dataKey={xAxisValues} tickMargin={20} />
+        <YAxis
+          yAxisId="kg"
+          dataKey="kg"
+          type="number"
+          domain={["dataMin - 10", "dataMax + 10"]}
+          orientation="right"
+        />
+        <YAxis
+          yAxisId="cal"
+          dataKey="cal"
+          hide={true}
+          domain={["dataMin - 70", "dataMax + 70"]}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Bar
+          dataKey="kg"
+          barSize={7}
+          fill="#282d30"
+          yAxisId="kg"
+          shape={<BarShape />}
+          activeBar={<BarShape />}
+        />
+        <Bar
+          dataKey="cal"
+          barSize={7}
+          fill="#e60000"
+          yAxisId="cal"
+          shape={<BarShape />}
+          activeBar={<BarShape />}
+        />
+      </BarChart>
+    </article>
   )
 }
 
