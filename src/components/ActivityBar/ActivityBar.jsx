caa414a4db7c324b7a2679e1ab0bab9aa4da03e0
empostigo <emmanuel.postigo@tutanota.com>
2023-12-26 17:17:50 +0100

    Create legend

--- src/components/ActivityBar/ActivityBar.jsx
+++ src/components/ActivityBar/ActivityBar.jsx
@@ -8,7 +8,8 @@ import {
   Line,
   Tooltip,
   Bar,
-  ResponsiveContainer
+  ResponsiveContainer,
+  Legend
 } from "recharts"
 
 // Components
@@ -19,7 +20,20 @@ import CustomTooltip from "../CustomTooltip/CustomTooltip"
 import activityStyle from "./ActivityBar.module.scss"
 
 const ActivityBar = ({ data }) => {
+  // Transform dates to numbers
   const xAxisValues = () => data.map(activity => data.indexOf(activity) + 1)
+
+  // Formatter for legend
+  const renderLegend = (value, entry, index) => {
+    const labels = ["Poids (", "Calories brûlées (k"]
+    const values = [value, value.charAt(0).toUpperCase() + value.slice(1)]
+
+    return (
+      <span
+        className={activityStyle.legend}
+      >{`${labels[index]}${values[index]})`}</span>
+    )
+  }
 
   return (
     <article className={activityStyle.article}>
@@ -43,6 +57,14 @@ const ActivityBar = ({ data }) => {
           />
           <YAxis yAxisId="cal" dataKey="cal" hide={true} />
           <Tooltip content={<CustomTooltip />} />
+          <Legend
+            verticalAlign="top"
+            align="right"
+            iconType="circle"
+            iconSize={8}
+            formatter={renderLegend}
+            margin={{ top: 100, left: 0, right: 0, bottom: 100 }}
+          />
           <Bar
             dataKey="kg"
             barSize={7}
