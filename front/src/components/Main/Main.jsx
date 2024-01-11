b58d476ee13cd9441adf50db564c0ee54170c2a1
empostigo <emmanuel.postigo@tutanota.com>
2024-01-11 11:37:02 +0100

    Create Main component

---
+++ front/src/components/Main/Main.jsx
@@ -0,0 +1,57 @@
+// Components
+import NavBar from "../NavBar/NavBar"
+import ActivityBar from "../ActivityBar/ActivityBar"
+import SessionsTime from "../SessionsTime/SessionsTime"
+import PerformanceRadar from "../PerformanceRadar/PerformanceRadar"
+import Capsule from "../Capsule/Capsule"
+import Score from "../Score/Score"
+
+// Assets
+import calories from "../../assets/data-icons/calories-icon.svg"
+import proteins from "../../assets/data-icons/protein-icon.svg"
+import carbs from "../../assets/data-icons/carbs-icon.svg"
+import lipids from "../../assets/data-icons/fat-icon.svg"
+
+// Style
+import mainStyle from "./Main.module.scss"
+
+const Main = ({ data }) => {
+  const { userInfos, activity, sessions, performance, score, keyData } = data
+  const iconsArray = [calories, proteins, carbs, lipids]
+
+  return (
+    <main className={mainStyle.main}>
+      <NavBar />
+      <section className={mainStyle.userInfos}>
+        <h1 className={mainStyle.h1}>
+          bonjour{" "}
+          <span className={mainStyle.userName}>{`${userInfos.firstName}`}</span>
+        </h1>
+        <p className={mainStyle.congrats}>
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </p>
+      </section>
+      <section className={mainStyle.chartWrapper}>
+        <div className={mainStyle.charts}>
+          <ActivityBar data={activity} />
+          <div className={mainStyle.smallCharts}>
+            <SessionsTime data={sessions} />
+            <PerformanceRadar data={performance} />
+            <Score data={score} />
+          </div>
+        </div>
+        <div className={mainStyle.capsules}>
+          {keyData.map((item, index) => (
+            <Capsule
+              key={`item-${index}`}
+              icon={iconsArray[index]}
+              data={item}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Main
--- front/src/components/Root/Root.jsx
+++ front/src/components/Root/Root.jsx
@@ -6,22 +6,8 @@ import axios from "axios"
 
 // Components
 import Header from "../Header/Header"
-import NavBar from "../NavBar/NavBar"
-import ActivityBar from "../ActivityBar/ActivityBar"
-import SessionsTime from "../SessionsTime/SessionsTime"
-import PerformanceRadar from "../PerformanceRadar/PerformanceRadar"
-import Capsule from "../Capsule/Capsule"
-import Score from "../Score/Score"
+import Main from "../Main/Main"
 import Footer from "../Footer/Footer"
-
-// Assets
-import calories from "../../assets/data-icons/calories-icon.svg"
-import proteins from "../../assets/data-icons/protein-icon.svg"
-import carbs from "../../assets/data-icons/carbs-icon.svg"
-import lipids from "../../assets/data-icons/fat-icon.svg"
-
-// Style
-import rootStyle from "./Root.module.scss"
 
 const Root = () => {
   const [data, setData] = useState(null)
@@ -38,45 +24,10 @@ const Root = () => {
     return <p>Chargement en cours...</p>
   }
 
-  const { userInfos, activity, sessions, performance, score, keyData } = data
-  const iconsArray = [calories, proteins, carbs, lipids]
-
   return (
     <>
       <Header />
-      <main className={rootStyle.main}>
-        <NavBar />
-        <section className={rootStyle.userInfos}>
-          <h1 className={rootStyle.h1}>
-            bonjour{" "}
-            <span
-              className={rootStyle.userName}
-            >{`${userInfos.firstName}`}</span>
-          </h1>
-          <p className={rootStyle.congrats}>
-            Félicitation ! Vous avez explosé vos objectifs hier 👏
-          </p>
-        </section>
-        <section className={rootStyle.chartWrapper}>
-          <div className={rootStyle.charts}>
-            <ActivityBar data={activity} />
-            <div className={rootStyle.smallCharts}>
-              <SessionsTime data={sessions} />
-              <PerformanceRadar data={performance} />
-              <Score data={score} />
-            </div>
-          </div>
-          <div className={rootStyle.capsules}>
-            {keyData.map((item, index) => (
-              <Capsule
-                key={`item-${index}`}
-                icon={iconsArray[index]}
-                data={item}
-              />
-            ))}
-          </div>
-        </section>
-      </main>
+      <Main data={data} />
       <Footer />
     </>
   )
